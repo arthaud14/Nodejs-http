c5f31f4c6a435cd7cc2a8ebabe251aead21441ca
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-10-30 19:03:39 +0200

    changed code

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ const port = 8000;
 
 const requestHandler = (request, response) => {
     console.log(request.url);
-    response.end('Hello, <name> from <city>!');
+    const parsedUrl.parse(request.url, true);
+
+    const name = parsedUrl.query.name;
+    const city = parsedUrl.query.city;
+
+    if(name && city) {
+      response.end(`Hello ${name} from ${city}`);
+    } else {
+      response.end('Please provide name and city parameters');
+    }
   };
 
   const server = http.createServer(requestHandler);
