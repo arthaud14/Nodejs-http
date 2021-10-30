8b61a760ed41f06e7f337e5e234c1bf095820292
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-10-30 19:16:29 +0200

    add const url

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
-const http = require('http');
+const http = require("http");
+const url = require('url');
 const port = 8000;
 
 const requestHandler = (request, response) => {
-    console.log(request.url);
-    const parsedUrl = parse(request.url, true);
+  const getUrl = request.url;
+  const parsedUrl = parse(request.url, true);
 
-    const name = parsedUrl.query.name;
-    const city = parsedUrl.query.city;
+  const name = parsedUrl.query.name;
+  const city = parsedUrl.query.city;
 
-    if(name && city) {
-      response.end(`Hello ${name} from ${city}`);
-    } else {
-      response.end('Please provide name and city parameters');
-    }
-  };
+  if (name && city) {
+    response.end(`Hello ${name} from ${city}`);
+  } else {
+    response.end("Please provide name and city parameters");
+  }
+};
 
-  const server = http.createServer(requestHandler);
+const server = http.createServer(requestHandler);
 
-  server.listen(port, (err) => {
-    if (err) {
-      console.error('Something bad happened');
-    } else {
-      console.log(`server is listening on ${port}`);
-    }
-  });
+server.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`server is listening on ${port}`);
+  }
+});
