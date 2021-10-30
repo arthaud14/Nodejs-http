f1d1637a192fecc4ad081f14c06fe96fd773a13c
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2021-10-30 18:52:09 +0200

    transferd to github

---
+++ server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const port = 8000;
+
+const requestHandler = (request, response) => {
+    console.log(request.url);
+    response.end('Hello, <name> from <city>!');
+  };
+
+  const server = http.createServer(requestHandler);
+
+  server.listen(port, (err) => {
+    if (err) {
+      console.error('Something bad happened');
+    } else {
+      console.log(`server is listening on ${port}`);
+    }
+  });
